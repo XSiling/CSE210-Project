9d39ae0b183ccbbd4ba473ec914a1d0bb6bfa208
Shubham <snpatil@ucsd.edu>
2023-11-28 23:17:28 -0800

    added Apis for rec sys

---
+++ src/mastodonApi/api.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+import json
+from updateRecommendations import *
+import threading
+
+app = Flask(__name__)
+
+@app.route('/get_recommendations', methods=['GET'])
+def get_recommendations():
+    with open('./recommendations.json', 'r') as file:
+        data = json.load(file)
+    return jsonify(data)
+
+
+@app.route('/get_recommendedpeople', methods=['GET'])
+def get_recommendedpeople():
+    userMastodonURL = request.args.get('userMastodonURL')
+    return jsonify(recommendPeople(userMastodonURL))
+
+
+if __name__ == '__main__':
+    recThread = threading.Thread(target=updateRecs)
+    recThread.start()
+    app.run(port=5000)
+    
