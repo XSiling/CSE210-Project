88cc3a59ab2550d62690cb41531ffbb9882ec423
Niko-Yuan <hairforcenikoyuan@gmail.com>
2023-11-29 05:26:44 -0800

    update recommendation page
    1) make card component
    2) add recommendation card
    3) make api.py support CORS

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,3 @@
-//Fetch data from backend
-import axios from "axios";
+const apiKey = "http://localhost:5000";
 
-axios.get('/api/group')
-  .then(response => console.log(response.data))
-  .catch(error => console.error('Error:', error));
-
-axios.get('/api/post')
-  .then(response => console.log(response.data))
-  .catch(error => console.error('Error:', error));
-
-axios.get('/api/person')
-  .then(response => console.log(response.data))
-  .catch(error => console.error('Error:', error));
-
-axios.get('/api/profile')
-  .then(response => console.log(response.data))
-  .catch(error => console.error('Error:', error));
+export default apiKey;
--- src/mastodonApi/api.py
+++ src/mastodonApi/api.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import json
 from updateRecommendations import *
 import threading
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/get_recommendations', methods=['GET'])
 def get_recommendations():
@@ -11,12 +13,10 @@ def get_recommendations():
         data = json.load(file)
     return jsonify(data)
 
-
 @app.route('/get_recommendedpeople', methods=['GET'])
 def get_recommendedpeople():
     userMastodonURL = request.args.get('userMastodonURL')
     return jsonify(recommendPeople(userMastodonURL))
-
 
 if __name__ == '__main__':
     recThread = threading.Thread(target=updateRecs)
