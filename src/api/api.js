841b90d4a038a6a07eed25f5ab00bcf31ada3bc0
Niko-Yuan <hairforcenikoyuan@gmail.com>
2023-12-01 17:53:47 -0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 // Replace the url of the server when deployed
 const flaskApikey = "http://localhost:5000";
-const nodeApikey = "http://localhost:5000";
+const nodeApikey = "http://localhost:3000";
 
 export { flaskApikey, nodeApikey };
