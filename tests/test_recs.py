f3f44971913b5a595bc39e215009aa078b408b8e
Varun Patro <varunpatro17@gmail.com>
2023-12-02 13:42:29 -0800

    first test on searchInterest

--- tests/test_recs.py
+++ tests/test_recs.py
@@ -1,8 +1,26 @@
 import unittest
-import mastodonApi.search
-import mastodonApi.login
-import mastodonApi.registerApp
+import os
+import sys
+path = '../src/mastodonApi'
+sys.path.insert(1, path)
+from search import *
+
+
+from mastodon.utility import AttribAccessDict
+
 
 class TestAll(unittest.TestCase):
 
-    def test_search(self):
+    def test_searchInterest(self):
+        interest = 'travel'
+        bestAccounts, bestPosts = searchInterest(interest)
+        for i in range(len(bestAccounts)):
+            account = bestAccounts[i]
+            self.assertIsInstance(account, AttribAccessDict)
+
+        for i in range(len(bestPosts)):
+            post = bestPosts[i]
+            self.assertIsInstance(post, AttribAccessDict)
+
+if __name__ == '__main__':
+    unittest.main()
