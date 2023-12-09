d46dab6177eae2ee1ae5ccbe2b09580c7f02e245
Niko-Yuan <hairforcenikoyuan@gmail.com>
2023-12-09 06:29:24 -0800

    update get credential

--- src/components/recommendation.js
+++ src/components/recommendation.js
@@ -1,64 +1,62 @@
 import { renderPeopleRecommendation } from "./card.js";
 import { renderRecommendationPost } from "./post.js";
 import { renderFollowerRecommendation } from "./account.js";
-import { flaskApikey, nodeApikey } from '../api/api.js';
+import { flaskApikey, nodeApikey } from "../api/api.js";
 
 // TODO
 // 1) Merge fetchFollower and fetchPost into one promise
 
 function showLoadingGif(containerId) {
   const container = document.getElementById(containerId);
-  const loadingGif = document.createElement('img');
-  loadingGif.setAttribute('src', "../images/loading.gif");
-  loadingGif.setAttribute('id', 'loadingGif');
-  container.innerHTML = '';
+  const loadingGif = document.createElement("img");
+  loadingGif.setAttribute("src", "../images/loading.gif");
+  loadingGif.setAttribute("id", "loadingGif");
+  container.innerHTML = "";
   container.appendChild(loadingGif);
 }
 
 function hideLoadingGif(containerId) {
   const container = document.getElementById(containerId);
-  const loadingGif = document.getElementById('loadingGif');
+  const loadingGif = document.getElementById("loadingGif");
   if (loadingGif) {
     container.removeChild(loadingGif);
   }
 }
 
-function fetchBasicInformation(user){
+function fetchBasicInformation(user) {
   const username = user.username;
   const interests = user.interests;
   const mastodonAccount = user.mastodonAccount;
   const profile_img = user.profile_img;
-  let html = '';
-  
+  let html = "";
+
   // set username
   document.getElementById("username").setAttribute("value", username);
-  if (document.getElementById("userProfileUsername")){
-      document.getElementById("userProfileUsername").innerHTML = username;
+  if (document.getElementById("userProfileUsername")) {
+    document.getElementById("userProfileUsername").innerHTML = username;
   }
-  
+
   // set mastodon Account
-  document.getElementById("userProfileMastodonAccount").innerHTML = "<div>" + mastodonAccount +  "</div>"
+  document.getElementById("userProfileMastodonAccount").innerHTML =
+    "<div>" + mastodonAccount + "</div>";
 
   // set interests text
-  Array.from(interests).forEach(interest=>{
-    html += '<div class=interestsLi>' + interest + '</div>';
+  Array.from(interests).forEach((interest) => {
+    html += "<div class=interestsLi>" + interest + "</div>";
   });
-  document.getElementById("userProfileInterests").innerHTML =  html;
-
+  document.getElementById("userProfileInterests").innerHTML = html;
 
   // set image
   const container = document.getElementById("userProfileImage");
   let profile_image = document.createElement("img");
   profile_image.src = profile_img;
   let smt = profile_image.src;
-  const image_url = smt.replace('/view', '/images/user-image');
-  profile_image.src = image_url+'.png';
+  const image_url = smt.replace("/view", "/images/user-image");
+  profile_image.src = image_url + ".png";
   profile_image.alt = "profile image";
   profile_image.className = "profile-image";
-  container.appendChild(profile_image);  
-
+  container.appendChild(profile_image);
 }
-
 
 async function fetchFollowerRecommendations(interests) {
   fetch(`${flaskApikey}/get_recommendations`)
@@ -71,20 +69,20 @@ async function fetchFollowerRecommendations(interests) {
     .then((data) => {
       const container = document.getElementById("accountContainer");
       container.innerHTML = "";
-      const header = document.createElement('h3');
+      const header = document.createElement("h3");
       header.textContent = "Top Recommended Followers";
       container.appendChild(header);
       if (Array.isArray(interests)) {
         interests.forEach((interest) => {
-          if(data.hasOwnProperty(interest)) {
+          if (data.hasOwnProperty(interest)) {
             const recommendData = data[interest];
             const top2Account = recommendData.top2FollowedAccounts;
             top2Account.forEach((account) => {
-                let return_account = renderFollowerRecommendation(account);
-                container.appendChild(return_account);
-            })
+              let return_account = renderFollowerRecommendation(account);
+              container.appendChild(return_account);
+            });
           } else {
-            console.log('No data found for:', interest);
+            console.log("No data found for:", interest);
           }
         });
       } else {
@@ -110,20 +108,20 @@ async function fetchPostRecommendations(interests) {
     .then((data) => {
       const container = document.getElementById("postContainer");
       container.innerHTML = "";
-      const header = document.createElement('h3');
+      const header = document.createElement("h3");
       header.textContent = "Top Recommended Post";
       container.appendChild(header);
       if (Array.isArray(interests)) {
         interests.forEach((interest) => {
-          if(data.hasOwnProperty(interest)) {
+          if (data.hasOwnProperty(interest)) {
             const recommendData = data[interest];
             const top2Posts = recommendData.top2Posts;
             top2Posts.forEach((post) => {
-                let return_post = renderRecommendationPost(post);
-                container.appendChild(return_post);
-            })
+              let return_post = renderRecommendationPost(post);
+              container.appendChild(return_post);
+            });
           } else {
-            console.log('No data found for:', interest);
+            console.log("No data found for:", interest);
           }
         });
       } else {
@@ -154,7 +152,7 @@ async function fetchPeopleRecommended(userMastodonURL) {
     .then((data) => {
       const container = document.getElementById("recommendationContainer");
       container.innerHTML = "";
-      const header = document.createElement('h3');
+      const header = document.createElement("h3");
       header.textContent = "People You Might Know";
       container.appendChild(header);
       data.forEach((userData) => {
@@ -170,75 +168,79 @@ async function fetchPeopleRecommended(userMastodonURL) {
 // Fetch interest and user account infos
 async function fetchUserData() {
   // Show loading GIFs
-  showLoadingGif('accountContainer');
-  showLoadingGif('postContainer');
-  showLoadingGif('recommendationContainer');
+  showLoadingGif("accountContainer");
+  showLoadingGif("postContainer");
+  showLoadingGif("recommendationContainer");
   try {
-      const response = await fetch(`${nodeApikey}/users`);
-      if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      const data = await response.json();
-      const username = window.location.href.split('=')[1].split('&')[0];
+    const response = await fetch(`${nodeApikey}/users`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    const username = window.location.href.split("=")[1].split("&")[0];
 
-      let i = -1;
-      for(let index=0; index<data.users.length; ++index){
-        if (data.users[index].username === username){
-            i = index;
-            break;
-        }
+    let i = -1;
+    for (let index = 0; index < data.users.length; ++index) {
+      if (data.users[index].username === username) {
+        i = index;
+        break;
       }
+    }
 
-      // set the basic information on the page
-      fetchBasicInformation(data.users[i]);
+    // set the basic information on the page
+    fetchBasicInformation(data.users[i]);
 
-      if (data.users[i].interests && Array.isArray(data.users[i].interests) && data.users[i].interests.length > 0) {
-        const interest = data.users[i].interests;
-        await fetchFollowerRecommendations(interest);
-        await fetchPostRecommendations(interest);
-      }
-      if (data.users[i].mastodonAccount?.trim()) {
-        const userMastodonURL = data.users[i].mastodonAccount;
-        await fetchPeopleRecommended(userMastodonURL);
-      }
-      console.log("render finish");
+    if (
+      data.users[i].interests &&
+      Array.isArray(data.users[i].interests) &&
+      data.users[i].interests.length > 0
+    ) {
+      const interest = data.users[i].interests;
+      await fetchFollowerRecommendations(interest);
+      await fetchPostRecommendations(interest);
+    }
+    if (data.users[i].mastodonAccount?.trim()) {
+      const userMastodonURL = data.users[i].mastodonAccount;
+      await fetchPeopleRecommended(userMastodonURL);
+    }
+    console.log("render finish");
   } catch (error) {
-      console.error('Error fetching user data:', error);
+    console.error("Error fetching user data:", error);
   } finally {
-    hideLoadingGif('accountContainer');
-    hideLoadingGif('postContainer');
-    hideLoadingGif('recommendationContainer');
+    hideLoadingGif("accountContainer");
+    hideLoadingGif("postContainer");
+    hideLoadingGif("recommendationContainer");
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById('logOutButton').addEventListener('click', () => {
+  document.getElementById("logOutButton").addEventListener("click", () => {
     fetch(`${nodeApikey}/logout`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                window.location.href = 'register.html' // Reload the page after logout
-            }
-        });
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          window.location.href = "register.html"; // Reload the page after logout
+        }
+      });
   });
-
   fetchUserData();
+  getCredential();
 });
 
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
   const openProfileButton = document.getElementById("openProfileButton");
   const url = window.location.href;
-  const username = url.split('=')[1].split('&')[0];
+  const username = url.split("=")[1].split("&")[0];
   openProfileButton.setAttribute("src", "profile.html?username=" + username);
   console.log(openProfileButton);
-  openProfileButton.addEventListener("click", ()=>{
+  openProfileButton.addEventListener("click", () => {
     const profileWindow = document.getElementById("profileEdittion");
-    profileWindow.style.display = 'block';
-  })
-})
+    profileWindow.style.display = "block";
+  });
+});
 
 async function getCredential() {
-  console.log("credential button clicked");
+  // fetch user data
   let mastodonAccount;
   try {
     const response = await fetch(`${nodeApikey}/users`);
@@ -247,61 +249,67 @@ async function getCredential() {
     }
     const data = await response.json();
     mastodonAccount = data?.users[0]?.mastodonAccount;
-    console.log(mastodonAccount);
-    if (mastodonAccount) {
-      try {
-        const response = await fetch(`${flaskApikey}/check_User_Isloggedin?userMastodonURL=${encodeURIComponent(mastodonAccount)}`)
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.text();
-        console.log(data);
-        if (data === "True") {
-          console.log("Credential Found");
-          alert("Already Credentialed");
-        } else {
-          try {
-            const response = await fetch(`${flaskApikey}/get_Auth_URL?userMastodonURL=${encodeURIComponent(mastodonAccount)}`)
-            if (!response.ok) {
-              throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            const data = await response.text();
-            window.open(data, '_blank');
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    } else {
-      console.log("Not Found");
-    }
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
   }
+  // check user is authorize to mastodon or not (refresh case)
   try {
-    const response = await fetch(`${flaskApikey}/check_User_Isloggedin?userMastodonURL=${encodeURIComponent(mastodonAccount)}`)
+    const response = await fetch(
+      `${flaskApikey}/check_User_Isloggedin?userMastodonURL=${encodeURIComponent(
+        mastodonAccount
+      )}`
+    );
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const data = await response.text();
-    const status_span = document.getElementById('user_status_span');
     if (data === "True") {
-      status_span.textContent = "True";
-    } else {
-      status_span.textContent = "False";
+      // console.log("already");
+      const btn = document.getElementById("get-credential-btn");
+      btn.textContent = 'Mastodon Credentialed';
+      return;
     }
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
+  }
+  // get auth
+  try {
+    const response = await fetch(
+      `${flaskApikey}/get_Auth_URL?userMastodonURL=${encodeURIComponent(
+        mastodonAccount
+      )}`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const authUrl = await response.text();
+    const authWindow = window.open(authUrl, "_blank");
+    const checkAuthWindow = setInterval(async () => {
+      if (authWindow.closed) {
+          clearInterval(checkAuthWindow);
+          try {
+            const response = await fetch(
+              `${flaskApikey}/check_User_Isloggedin?userMastodonURL=${encodeURIComponent(
+                mastodonAccount
+              )}`
+            );
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.text();
+            if (data === "True") {
+              const btn = document.getElementById("get-credential-btn");
+              btn.textContent = 'Mastodon Credentialed';
+            } else {
+              const btn = document.getElementById("get-credential-btn");
+              btn.textContent = 'Link to Mastodon';
+            }
+          } catch (error) {
+            console.error("Error fetching data:", error);
+          }
+        }
+    }, 500);
+  } catch (error) {
+    console.error("Error fetching data:", error);
   }
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-  const credentialButton = document.getElementById('get-credential-btn');
-  if (credentialButton) {
-      credentialButton.addEventListener('click', getCredential);
-  }
-  const status_span = document.getElementById('user_status_span');
-  status_span.textContent = 'None';
-});
