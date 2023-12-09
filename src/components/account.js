5cb1f0bc5e3ad015ba56c59755f0078d8fb971a0
Fangqi Yuan <fayuan@ucsd.edu>
2023-12-08 19:20:57 -0800

    update follow/unfollow btn function (#109)

--- src/components/account.js
+++ src/components/account.js
@@ -1,59 +1,127 @@
+import { flaskApikey, nodeApikey } from "../api/api.js";
+
 export function renderFollowerRecommendation(recommendationData) {
-  const card = document.createElement('section');
-  card.className = 'follower-card';
+  const card = document.createElement("section");
+  card.className = "follower-card";
 
-  const upperContainer = document.createElement('section');
-  const middleContainer = document.createElement('section');
-  const lowerContainer = document.createElement('section');
-  upperContainer.className = 'follower-card-upper-section';
-  middleContainer.className = 'follower-card-middle-section';
-  lowerContainer.className = 'follower-card-lower-section';
-  
-  const imgContainer = document.createElement('section');
-  const infoContainer = document.createElement('section');
-  imgContainer.className = 'follower-card-img-section';
-  infoContainer.className = 'follower-card-info-section';
+  const upperContainer = document.createElement("section");
+  const middleContainer = document.createElement("section");
+  const lowerContainer = document.createElement("section");
+  upperContainer.className = "follower-card-upper-section";
+  middleContainer.className = "follower-card-middle-section";
+  lowerContainer.className = "follower-card-lower-section";
 
-  const avatar = document.createElement('img');
+  const imgContainer = document.createElement("section");
+  const infoContainer = document.createElement("section");
+  imgContainer.className = "follower-card-img-section";
+  infoContainer.className = "follower-card-info-section";
+
+  const avatar = document.createElement("img");
   avatar.src = recommendationData.avatar;
-  avatar.alt = 'Avatar';
-  avatar.className = 'follower-card-avatar';
+  avatar.alt = "Avatar";
+  avatar.className = "follower-card-avatar";
 
-  const nameUsernameContainer = document.createElement('section');
-  const followersFollowingContainer = document.createElement('section');
-  nameUsernameContainer.className = 'follower-card-name-username-container';
-  followersFollowingContainer.className = 'follower-card-followers-following-container';
+  const nameUsernameContainer = document.createElement("section");
+  const followersFollowingContainer = document.createElement("section");
+  nameUsernameContainer.className = "follower-card-name-username-container";
+  followersFollowingContainer.className =
+    "follower-card-followers-following-container";
 
-  const name = document.createElement('p');
+  const name = document.createElement("p");
   name.textContent = recommendationData.display_name;
-  name.className = 'follower-card-name';
+  name.className = "follower-card-name";
 
-  const username = document.createElement('p');
+  const username = document.createElement("p");
   username.textContent = `(${recommendationData.username})`;
-  username.className = 'follower-card-username';
+  username.className = "follower-card-username";
 
-  const followers = document.createElement('p');
+  const followers = document.createElement("p");
   followers.textContent = `Followers: ${recommendationData.followers_count}|`;
-  followers.className = 'follower-card-followers';
+  followers.className = "follower-card-followers";
 
-  const following = document.createElement('p');
+  const following = document.createElement("p");
   following.textContent = `Following: ${recommendationData.following_count}`;
-  followers.className = 'follower-card-following';
+  followers.className = "follower-card-following";
 
-  const bio = document.createElement('p');
+  const bio = document.createElement("p");
   bio.innerHTML = recommendationData.note;
-  bio.className = 'follower-card-bio';
+  bio.className = "follower-card-bio";
 
-  const profileLink = document.createElement('a');
+  const profileLink = document.createElement("a");
   profileLink.href = recommendationData.url;
-  profileLink.textContent = 'View Profile';
-  profileLink.className = 'follower-card-profile-link';
-  profileLink.target = '_blank';
-  profileLink.rel = 'noopener noreferrer';
+  profileLink.textContent = "View Profile";
+  profileLink.className = "follower-card-profile-link";
+  profileLink.target = "_blank";
+  profileLink.rel = "noopener noreferrer";
 
-  const follow_btn = document.createElement('a');
-  follow_btn.textContent = 'Follow him/her';
-  follow_btn.className = 'follower-card-follow-button';
+  const follow_btn = document.createElement("a");
+  follow_btn.textContent = "Follow him/her";
+  follow_btn.className = "follower-card-follow-button";
+
+  follow_btn.addEventListener("click", function () {
+    fetch(`${nodeApikey}/users`)
+      .then((response) => response.json())
+      .then((data) => {
+        const users = data.users;
+        const userToFollow = users[0];
+
+        const userMastodonURL = userToFollow?.mastodonAccount;
+        const followUserURL = recommendationData?.acct;
+
+        const followURL = `${flaskApikey}/follow_People?userMastodonURL=${encodeURIComponent(
+          userMastodonURL
+        )}&followUserURL=${encodeURIComponent(followUserURL)}`;
+
+        fetch(followURL)
+          .then((response) => response.text())
+          .then((result) => {
+            console.log("Follow action result:", result);
+            follow_btn.style.display = 'none';
+            unfollow_btn.style.display = 'block';
+          })
+          .catch((error) => {
+            console.error("Error following user:", error);
+          });
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  });
+
+  const unfollow_btn = document.createElement("a");
+  unfollow_btn.textContent = "UnFollow him/her";
+  unfollow_btn.className = "follower-card-unfollow-button";
+  unfollow_btn.style.display = 'none';
+
+  unfollow_btn.addEventListener("click", function () {
+    fetch(`${nodeApikey}/users`)
+      .then((response) => response.json())
+      .then((data) => {
+        const users = data.users;
+        const userToFollow = users[0];
+
+        const userMastodonURL = userToFollow?.mastodonAccount;
+        const followUserURL = recommendationData?.acct;
+
+        const followURL = `${flaskApikey}/unfollow_People?userMastodonURL=${encodeURIComponent(
+          userMastodonURL
+        )}&unfollowUserURL=${encodeURIComponent(followUserURL)}`;
+
+        fetch(followURL)
+          .then((response) => response.text())
+          .then((result) => {
+            console.log("unFollow action result:", result);
+            follow_btn.style.display = 'block';
+            unfollow_btn.style.display = 'none';
+          })
+          .catch((error) => {
+            console.error("Error following user:", error);
+          });
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  });
 
   imgContainer.appendChild(avatar);
   nameUsernameContainer.appendChild(name);
@@ -65,6 +133,7 @@ export function renderFollowerRecommendation(recommendationData) {
   middleContainer.appendChild(bio);
   lowerContainer.appendChild(profileLink);
   lowerContainer.appendChild(follow_btn);
+  lowerContainer.appendChild(unfollow_btn);
   upperContainer.appendChild(imgContainer);
   upperContainer.appendChild(infoContainer);
   card.appendChild(upperContainer);
