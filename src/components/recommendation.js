0d52e9bc56e2a72cbd67a540133671e690203f3c
Niko-Yuan <hairforcenikoyuan@gmail.com>
2023-12-10 03:05:22 -0800

    fix bug

--- src/components/recommendation.js
+++ src/components/recommendation.js
@@ -35,7 +35,7 @@ function hideLoadingGif(containerId) {
  * @param {any} user
  * @returns {any}
  */
-async function fetchBasicInformation(user){
+async function fetchBasicInformation(user) {
   const username = user.username;
   const interests = user.interests;
   const mastodonAccount = user.mastodonAccount;
@@ -57,10 +57,11 @@ async function fetchBasicInformation(user){
     html += interest + " ";
   });
 
-  html += "</h4>"
+  html += "</h4>";
 
-  if (interests.length == 0){
-    html = '<br/><h4> Oops, you did not select any interests! Get some by editting the profile. </h4>';
+  if (interests.length == 0) {
+    html =
+      "<br/><h4> Oops, you did not select any interests! Get some by editting the profile. </h4>";
   }
 
   document.getElementById("userProfileInterests").innerHTML = html;
@@ -70,14 +71,12 @@ async function fetchBasicInformation(user){
   let profile_image = document.createElement("img");
   profile_image.src = profile_img;
   let smt = profile_image.src;
-  const image_url = smt.replace('/view', '/images/user-image');
-  profile_image.src = image_url+'.png';
+  const image_url = smt.replace("/view", "/images/user-image");
+  profile_image.src = image_url + ".png";
   profile_image.alt = "profile image";
   profile_image.className = "profile-image";
-  container.appendChild(profile_image);  
-
+  container.appendChild(profile_image);
 }
-
 
 /**
  * Description
@@ -255,9 +254,9 @@ async function fetchUserData() {
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("logOutButton").addEventListener("click", () => {
     fetch(`${nodeApikey}/logout`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
     })
       .then((response) => response.json())
@@ -272,26 +271,26 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 window.onload = function () {
-  if (sessionStorage.getItem('loginSuccess') === 'true') {
-    sessionStorage.removeItem('loginSuccess');
+  if (sessionStorage.getItem("loginSuccess") === "true") {
+    sessionStorage.removeItem("loginSuccess");
     const Toast = Swal.mixin({
       toast: true,
-      position: 'top-end',
+      position: "top-end",
       showConfirmButton: false,
       timer: 3000,
       timerProgressBar: true,
       didOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer);
-        toast.addEventListener('mouseleave', Swal.resumeTimer);
-      }
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
     });
 
     Toast.fire({
-      icon: 'success',
-      title: 'Signed in successfully'
+      icon: "success",
+      title: "Signed in successfully",
     });
   }
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   const openProfileButton = document.getElementById("openProfileButton");
@@ -319,8 +318,10 @@ async function getCredential() {
     }
     const data = await response.json();
     const urlParams = new URLSearchParams(window.location.search);
-    const username = urlParams.get('username');
-    const userIndex = data.users.findIndex(user => user.username === username);
+    const username = urlParams.get("username");
+    const userIndex = data.users.findIndex(
+      (user) => user.username === username
+    );
     mastodonAccount = data?.users[userIndex]?.mastodonAccount;
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -339,7 +340,9 @@ async function getCredential() {
     if (data === "True") {
       // console.log("already");
       const btn = document.getElementById("get-credential-btn");
-      btn.textContent = 'Mastodon Credentialed';
+      btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mastodon" viewBox="0 0 16 16">
+      <path d="M11.19 12.195c2.016-.24 3.77-1.475 3.99-2.603.348-1.778.32-4.339.32-4.339 0-3.47-2.286-4.488-2.286-4.488C12.062.238 10.083.017 8.027 0h-.05C5.92.017 3.942.238 2.79.765c0 0-2.285 1.017-2.285 4.488l-.002.662c-.004.64-.007 1.35.011 2.091.083 3.394.626 6.74 3.78 7.57 1.454.383 2.703.463 3.709.408 1.823-.1 2.847-.647 2.847-.647l-.06-1.317s-1.303.41-2.767.36c-1.45-.05-2.98-.156-3.215-1.928a3.614 3.614 0 0 1-.033-.496s1.424.346 3.228.428c1.103.05 2.137-.064 3.188-.189zm1.613-2.47H11.13v-4.08c0-.859-.364-1.295-1.091-1.295-.804 0-1.207.517-1.207 1.541v2.233H7.168V5.89c0-1.024-.403-1.541-1.207-1.541-.727 0-1.091.436-1.091 1.296v4.079H3.197V5.522c0-.859.22-1.541.66-2.046.456-.505 1.052-.764 1.793-.764.856 0 1.504.328 1.933.983L8 4.39l.417-.695c.429-.655 1.077-.983 1.934-.983.74 0 1.336.259 1.791.764.442.505.661 1.187.661 2.046v4.203z"/>
+    </svg>Linked`;
       return;
     }
   } catch (error) {
@@ -359,58 +362,63 @@ async function getCredential() {
     const authWindow = window.open(authUrl, "_blank");
     const checkAuthWindow = setInterval(async () => {
       if (authWindow.closed) {
-          clearInterval(checkAuthWindow);
-          try {
-            const response = await fetch(
-              `${flaskApikey}/check_User_Isloggedin?userMastodonURL=${encodeURIComponent(
-                mastodonAccount
-              )}`
-            );
-            if (!response.ok) {
-              throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            const data = await response.text();
-            if (data === "True") {
-              const btn = document.getElementById("get-credential-btn");
-              btn.textContent = "Mastodon Credentialed";
-            } else {
-              function showMastodonToast() {
-                const Toast = Swal.mixin({
-                  toast: true,
-                  position: "top-end",
-                  showConfirmButton: false,
-                  timer: 3000,
-                  timerProgressBar: true,
-                  didOpen: (toast) => {
-                    toast.addEventListener("mouseenter", Swal.stopTimer);
-                    toast.addEventListener("mouseleave", Swal.resumeTimer);
-                  },
-                });
-              
-                Toast.fire({
-                  icon: "error",
-                  title: "Please Connect to Mastodon!",
-                });
-              }
-              const btn = document.getElementById("get-credential-btn");
-              btn.textContent = "Link to Mastodon";
-              const followBtns1 = document.getElementsByClassName(
-                "follower-card-follow-button"
-              );
-              Array.from(followBtns1).forEach((followBtn) => {
-                followBtn.addEventListener('click', showMastodonToast);
-              });
-              const followBtns2 = document.getElementsByClassName(
-                "people-card-follow-button"
-              );
-              Array.from(followBtns2).forEach((followBtn) => {
-                followBtn.addEventListener('click', showMastodonToast);
-              });
-            }
-          } catch (error) {
-            console.error("Error fetching data:", error);
+        clearInterval(checkAuthWindow);
+        try {
+          const response = await fetch(
+            `${flaskApikey}/check_User_Isloggedin?userMastodonURL=${encodeURIComponent(
+              mastodonAccount
+            )}`
+          );
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
           }
+          const data = await response.text();
+          if (data === "True") {
+            const btn = document.getElementById("get-credential-btn");
+            btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mastodon" viewBox="0 0 16 16">
+              <path d="M11.19 12.195c2.016-.24 3.77-1.475 3.99-2.603.348-1.778.32-4.339.32-4.339 0-3.47-2.286-4.488-2.286-4.488C12.062.238 10.083.017 8.027 0h-.05C5.92.017 3.942.238 2.79.765c0 0-2.285 1.017-2.285 4.488l-.002.662c-.004.64-.007 1.35.011 2.091.083 3.394.626 6.74 3.78 7.57 1.454.383 2.703.463 3.709.408 1.823-.1 2.847-.647 2.847-.647l-.06-1.317s-1.303.41-2.767.36c-1.45-.05-2.98-.156-3.215-1.928a3.614 3.614 0 0 1-.033-.496s1.424.346 3.228.428c1.103.05 2.137-.064 3.188-.189zm1.613-2.47H11.13v-4.08c0-.859-.364-1.295-1.091-1.295-.804 0-1.207.517-1.207 1.541v2.233H7.168V5.89c0-1.024-.403-1.541-1.207-1.541-.727 0-1.091.436-1.091 1.296v4.079H3.197V5.522c0-.859.22-1.541.66-2.046.456-.505 1.052-.764 1.793-.764.856 0 1.504.328 1.933.983L8 4.39l.417-.695c.429-.655 1.077-.983 1.934-.983.74 0 1.336.259 1.791.764.442.505.661 1.187.661 2.046v4.203z"/>
+            </svg>Linked`;
+          } else {
+            function showMastodonToast() {
+              const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.addEventListener("mouseenter", Swal.stopTimer);
+                  toast.addEventListener("mouseleave", Swal.resumeTimer);
+                },
+              });
+
+              Toast.fire({
+                icon: "error",
+                title: "Please Connect to Mastodon!",
+              });
+            }
+            const btn = document.getElementById("get-credential-btn");
+            btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mastodon" viewBox="0 0 16 16">
+              <path d="M11.19 12.195c2.016-.24 3.77-1.475 3.99-2.603.348-1.778.32-4.339.32-4.339 0-3.47-2.286-4.488-2.286-4.488C12.062.238 10.083.017 8.027 0h-.05C5.92.017 3.942.238 2.79.765c0 0-2.285 1.017-2.285 4.488l-.002.662c-.004.64-.007 1.35.011 2.091.083 3.394.626 6.74 3.78 7.57 1.454.383 2.703.463 3.709.408 1.823-.1 2.847-.647 2.847-.647l-.06-1.317s-1.303.41-2.767.36c-1.45-.05-2.98-.156-3.215-1.928a3.614 3.614 0 0 1-.033-.496s1.424.346 3.228.428c1.103.05 2.137-.064 3.188-.189zm1.613-2.47H11.13v-4.08c0-.859-.364-1.295-1.091-1.295-.804 0-1.207.517-1.207 1.541v2.233H7.168V5.89c0-1.024-.403-1.541-1.207-1.541-.727 0-1.091.436-1.091 1.296v4.079H3.197V5.522c0-.859.22-1.541.66-2.046.456-.505 1.052-.764 1.793-.764.856 0 1.504.328 1.933.983L8 4.39l.417-.695c.429-.655 1.077-.983 1.934-.983.74 0 1.336.259 1.791.764.442.505.661 1.187.661 2.046v4.203z"/>
+            </svg> Click here to link Mastodon`;
+            btn.addEventListener("click", getCredential);
+            const followBtns1 = document.getElementsByClassName(
+              "follower-card-follow-button"
+            );
+            Array.from(followBtns1).forEach((followBtn) => {
+              followBtn.addEventListener("click", showMastodonToast);
+            });
+            const followBtns2 = document.getElementsByClassName(
+              "people-card-follow-button"
+            );
+            Array.from(followBtns2).forEach((followBtn) => {
+              followBtn.addEventListener("click", showMastodonToast);
+            });
+          }
+        } catch (error) {
+          console.error("Error fetching data:", error);
         }
+      }
     }, 500);
   } catch (error) {
     console.error("Error fetching data:", error);
