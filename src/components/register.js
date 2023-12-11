0eeb58b5f5e7c8a44e5d4edc3c5d54514d2dff2f
Niko-Yuan <hairforcenikoyuan@gmail.com>
2023-12-10 21:04:12 -0800

    update UI

--- src/components/register.js
+++ src/components/register.js
@@ -4,7 +4,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const passwordInput = document.getElementById("newPassword");
   const passwordLabel = document.querySelector(".label-password");
   const comfirmPasswordInput = document.getElementById("confirmPassword");
-  const comfirmPasswordLabel = document.querySelector(".label-comfirm-password");
+  const comfirmPasswordLabel = document.querySelector(
+    ".label-comfirm-password"
+  );
   const emailInput = document.getElementById("email");
   const emailLabel = document.querySelector(".label-email");
 
@@ -28,6 +30,28 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
   }
 
+  function handleComfirmPasswordInput() {
+    if (comfirmPasswordInput.value) {
+      comfirmPasswordLabel.style.opacity = 1;
+      comfirmPasswordLabel.style.maxHeight = "30px";
+    } else {
+      comfirmPasswordLabel.style.opacity = 0;
+      comfirmPasswordLabel.style.maxHeight = "0";
+    }
+  }
+
+  function handleEmailInput() {
+    if (emailInput.value) {
+      emailLabel.style.opacity = 1;
+      emailLabel.style.maxHeight = "30px";
+    } else {
+      emailLabel.style.opacity = 0;
+      emailLabel.style.maxHeight = "0";
+    }
+  }
+
   usernameInput.addEventListener("input", handleUsernameInput);
   passwordInput.addEventListener("input", handlePasswordInput);
+  comfirmPasswordInput.addEventListener("input", handleComfirmPasswordInput);
+  emailInput.addEventListener("input", handleEmailInput);
 });
