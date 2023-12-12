bb5787e5f418669ac5759170b76f9755fd1aab15
seanlindstrom <20287490+seanlindstrom@users.noreply.github.com>
2023-12-11 23:14:21 -0800

    added comments and docstrings (#149)
    Co-authored-by: Sean Lindstrom <seanlindstrom@Seans-MBP.lan1>

--- tests/test_recs.py
+++ tests/test_recs.py
@@ -6,6 +6,7 @@ import pytz
 import math
 import random
 
+# Adding 'src/mastodonApi' to the sys path to import modules
 path = 'src/mastodonApi'
 sys.path.insert(1, path)
 
@@ -13,6 +14,7 @@ sys.path.insert(1, path)
 from search import *
 from mastodon.utility import AttribAccessDict
 
+# Expected field lists for account and post objects
 expected_account_fields_22 = (
             ['id', 'username', 'acct', 'display_name', 'locked', 'bot', 
             'discoverable', 'group', 'created_at', 'note', 'url', 'uri', 'avatar', 
@@ -35,6 +37,16 @@ expected_post_fields = (
         )
 
 
+"""
+    Checks if the input dictionary contains expected fields.
+    
+    Args:
+    - input_dict (dict): The dictionary to be checked.
+    - expected_fields (list): List of expected fields.
+
+    Returns:
+    - bool: True if all expected fields are present, False otherwise.
+"""
 def has_expected_fields(input_dict, expected_fields):
     for field in input_dict.keys():
         if field not in expected_fields:
@@ -43,12 +55,15 @@ def has_expected_fields(input_dict, expected_fields):
 
 class TestAll(unittest.TestCase):
 
+    """
+        Test case for the searchInterest function.
+    """
     def test_searchInterest(self):
+        # Fetching best accounts and posts related to 'travel'
         interest = 'travel'
         bestAccounts, bestPosts = searchInterest(interest)
         
-        
-        
+        # Asserting fields and instance types for bestAccounts
         for i in range(len(bestAccounts)):
             account = bestAccounts[i]
             self.assertIsInstance(account, AttribAccessDict)
@@ -57,18 +72,25 @@ class TestAll(unittest.TestCase):
             elif len(account.keys()) == 24:
                 self.assertTrue(has_expected_fields(account, expected_account_fields_24))
 
+        # Asserting fields and instance types for bestPosts
         for i in range(len(bestPosts)):
             post = bestPosts[i]
             self.assertIsInstance(post, AttribAccessDict)
             self.assertEqual(28, len(post.keys()))
             self.assertTrue(has_expected_fields(post, expected_post_fields))
 
+
+    """
+        Test case for the calculate_activity function.
+    """
     def test_calculate_activity(self):
         date_format = '%Y-%m-%d %H:%M:%S'
 
+        # Creating test data for calculate_activity function
         user1_acct_dict = {'created_at': datetime.strptime('2022-11-08 00:00:00', date_format), 'statuses_count': 86}
         user2_acct_dict = {'created_at': datetime.strptime('2022-11-05 00:00:00', date_format), 'statuses_count': 0}
 
+        # Calculating activity and comparing with expected values
         current_datetime = datetime.now(pytz.UTC)
         time_created = user1_acct_dict['created_at']
         time_created = time_created.replace(tzinfo=pytz.UTC)
@@ -84,6 +106,10 @@ class TestAll(unittest.TestCase):
         activity_2 = calculate_activity(user2_acct_dict)
         self.assertEqual(activity_2,0)
 
+
+    """
+        Test case for the remove_nsfw_posts function.
+    """
     def test_remove_nsfw_posts(self):
         # Generate random test data
         sample_toot_list = [
@@ -108,8 +134,12 @@ class TestAll(unittest.TestCase):
 
         self.assertCountEqual(filtered_toots,manually_filtered)
         
-    
+
+    """
+        Test case for the recommendPeople function.
+    """
     def test_recommendPeople(self):
+        # Recommending people based on userMastodonURL and asserting fields and instance types
         userMastodonURL = '@stephenfry@mastodonapp.uk'
         recommendedPeople = recommendPeople(userMastodonURL)
 
